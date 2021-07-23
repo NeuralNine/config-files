e3555f31f319a8061fe60db60b95aa709b8bffcd
neuralnine <social@neuralnine.com>
2021-07-23 23:00:54 +0200

    Added Windows CMD Aliases

---
+++ aliases.bat
@@ -0,0 +1,9 @@
+@echo off
+
+:: Add key with name AutoRun to HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Command Processor
+:: Containing the full path
+
+DOSKEY pydir=cd "C:\Users\flori\Desktop\Programming\Python"
+DOSKEY neuraldir=cd "C:\Users\flori\Desktop\Programming\Python\NeuralNine"
+DOSKEY desk=cd "C:\Users\flori\Desktop"
+DOSKEY videodir=cd "C:\Users\flori\Desktop\Programming\Python\NeuralNine\videos"
