80b778c53175ca8dd08290b38c8bdb9a0e13c8d9
NeuralNine <social@neuralnine.com>
2024-12-20 02:44:30 +0100

    added user to p10k

--- .p10k.zsh
+++ .p10k.zsh
@@ -31,6 +31,7 @@
   # The list of segments shown on the left. Fill it with the most important segments.
   typeset -g POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(
     os_icon                 # os identifier
+	user
     dir                     # current directory
     vcs                     # git status
     prompt_char             # prompt symbol
